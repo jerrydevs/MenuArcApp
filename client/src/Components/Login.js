9c9bca0884fdc9f8a5b737cee64caf6feb53de77
Eric <dudals2900@gmail.com>
2020-10-21 12:51:42 -0400

    Login function

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -5,4 +5,25 @@ class Login extends Component {
         super();
         this.state = { token: '' };
     }
+
+    // #1 Called right after component mount.
+    componentDidMount() {
+        this.googleSDK()
+    }
+
+    // #4 Login function implementation
+    login = () => {
+        this.auth2.signIn().then(googleUser => {
+            let profile = googleUser.getBasicProfile();
+            console.log('Token || ' + googleUser.getAuthResponse().id_token);
+            console.log('ID: ' + profile.getId());
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+
+            this.setState({ token: googleUser.getAuthResponse().id_token });
+        });
+    }
 }
+
+export default Login;
